ad32545ed727905f099a7dc43fd1fbbf8e5f5e2d
aluno <aluno@ifsc.edu.br>
2023-11-22 13:32:50 -0300

    favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Orientado a Objetos</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="iflogo.png">
 </head>
 
 <body>
